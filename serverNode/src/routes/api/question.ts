79ad12c11c3d8d747b00bacab16c485fa9dc547b
Maxim Volchinskiy <thyxaxa@gmail.com>
2022-04-16 22:37:31 +0300

    test commit

--- serverNode/src/routes/api/question.ts
+++ serverNode/src/routes/api/question.ts
@@ -7,4 +7,6 @@ questionRouter.post("/", (req, res) => res.send("hallo"));
 questionRouter.put("/", (req, res) => res.send("hallo"));
 questionRouter.delete("/", (req, res) => res.send("hallo"));
 
+// test commit
+
 export default questionRouter;
