0342ee256b4ed8f21e053eb06ec8795a59b93264
Maxim Volchinskiy <thyxaxa@gmail.com>
2022-04-19 12:37:24 +0300

    add tslint

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
+import React from "react";
 
-import Header from './components/Header/Header';
-import Content from './components/Content/Content';
-import AddQuestionPopup from './components/AddQuestionPopup/AddQuestionPopup';
+import Header from "./components/Header/Header";
+import Content from "./components/Content/Content";
+import AddQuestionPopup from "./components/AddQuestionPopup/AddQuestionPopup";
 
 function App() {
-  
+
   return (
     <div className="App">
       <Header />
