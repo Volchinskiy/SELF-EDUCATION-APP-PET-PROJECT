6430040766f322bb32d32995e5a57cb0bc3f4fea
Maxim Volchinskiy <thyxaxa@gmail.com>
2022-05-03 17:21:10 +0300

    add area wich adding questions

--- htmlLayout/js/script.js
+++ htmlLayout/js/script.js
@@ -5,3 +5,9 @@ document.querySelector(".header__left-side-menu-button").addEventListener("click
   content.classList.toggle('--left-side-show');
   rightSide.classList.toggle('--right-side-not-stretched');
 })
+
+document.querySelector(".content__left-side-button-add-question").addEventListener("click", () => {
+  const content = document.querySelector(".content");
+
+  content.classList.toggle("--short-content");
+})
