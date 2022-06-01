ff52f71e04c90b4ac6302ce59b88e2a882431989
Maxim Volchinskiy <thyxaxa@gmail.com>
2022-06-01 23:35:43 +0300

    some commit

--- server-nest/src/app.module.ts
+++ server-nest/src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatsController } from './cats/cats.controller';
+import { QuestionModule } from './question/question.module';
+import { QuestionModule } from './question/question.module';
 
 @Module({
-  imports: [],
+  imports: [QuestionModule],
   controllers: [AppController, CatsController],
   providers: [AppService],
 })
