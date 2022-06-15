6cfda6710c19a16823dea553446bad3bf9bd3aa8
Maxim Volchinskiy <thyxaxa@gmail.com>
2022-06-15 16:25:50 +0300

    connected to database

--- server-nest/src/main.ts
+++ server-nest/src/main.ts
@@ -1,12 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import 'reflect-metadata';
+import AppDataSource from './database/data-source';
 
 import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   await app.listen(5000);
+
+  await AppDataSource.initialize()
+    .then(() => {
+      console.log('Connected To Database!');
+    })
+    .catch((err) => {
+      console.error("Didn't Connect To Database.", err);
+    });
+
   console.info(`Application Running On: ${await app.getUrl()}`);
 }
 bootstrap();
